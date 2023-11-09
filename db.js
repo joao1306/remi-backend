6e86b43a54b2ed11cb243b3aa22def0a8a716eaa
joao1306 <joaovictor1306@gmail.com>
2023-11-09 16:52:34 -0300

    test

--- db.js
+++ db.js
@@ -3,13 +3,8 @@ import mysql from 'mysql';
 
 export const db = mysql.createConnection({
     host: 'aws.connect.psdb.cloud',
-<<<<<<< HEAD
     user: 'ndq3w5k42t4us3gua8qo',
     password: 'pscale_pw_zHfAaBlV0QY7ybcIL1MHHYAm1vHtV8ewVEsrO3SZ4Pn',
-=======
-    user: 'u0mhm4yduhdipveil2ob',
-    password: 'pscale_pw_74jk2BKFIJCty3YKAZLy5Z9WByfjgC3mo2R72tVxPtz',
->>>>>>> ae05b3710d958db85a663f893b9fffb6f855a6aa
     database: 'remidb',
     ssl: {
       rejectUnauthorized: false
